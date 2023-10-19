5d85a05f2e993f5532627da004b84c586ddeb827
ensar-hodzic <hodzicensar@gmail.com>
2023-10-19 21:55:46 +0200

    Laboratorijska vjezba 1

--- LAB1/Z3/src/ba/unsa/etf/rpr/predavanje02/Main.java
+++ LAB1/Z3/src/ba/unsa/etf/rpr/predavanje02/Main.java
@@ -1,41 +1,40 @@
 package ba.unsa.etf.rpr.predavanje02;
 
-public class Sat{
-    int sati, minute, sekunde;
-    public:
+class Sat{
+    static int sati, minute, sekunde;
     Sat(int h, int m, int s){
         Postavi(h,m,s);
     }
-    static void Postavi(int h, int m, int s){
+    void Postavi(int h, int m, int s){
         sati=h; minute=m; sekunde=s;
     }
-    static void Sljedeci() {
+    void Sljedeci() {
         sekunde++;
         if (sekunde==60) { sekunde=0; minute++; }
         if (minute==60) { minute=0; sati++; }
         if (sati==24) sati=0;
     }
-    static void Prethodni() {
+    void Prethodni() {
         sekunde--;
         if (sekunde==-1) { sekunde=59; minute--; }
         if (minute==-1) { minute=59; sati--; }
         if (sati==-1) sati=23;
     }
-    static void PomjeriZa(int pomak) {
-        if (pomak>0) for (int i(0); i<pomak; i++) Sljedeci();
-                        else for (int i(0); i<-pomak; i++) Prethodni();
+    void PomjeriZa(int pomak) {
+        if (pomak>0) for (int i=0; i<pomak; i++) Sljedeci();
+                        else for (int i=0; i<-pomak; i++) Prethodni();
     }
-    static int DajSate() const { return sati; }
-    static int DajMinute() const { return minute; }
-    static int DajSekunde() const { return sekunde; }
-    static void Ispisi() const { System.out.print(sati+":"+minute+":"+sekunde) }
+    int DajSate() { return sati; }
+    int DajMinute() { return minute; }
+    int DajSekunde() { return sekunde; }
+    void Ispisi() { System.out.println(sati+":"+minute+":"+sekunde) ;}
 
 }
 
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        Sat s (15, 30, 45);
+        Sat s=new Sat(10,23,13);
         s.Ispisi();
         s.Sljedeci();
         s.Ispisi();
