3f915dc7d1f0b99b3884a76635e440c587836555
ensar-hodzic <hodzicensar@gmail.com>
2023-10-31 22:24:36 +0100

    lv2-z2

---
+++ lv2-z2/src/main/java/ba/unsa/etf/rpr/App.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Osoba o = new Osoba("Ensar", "Hodzic");
+        Uposlenik u = new Uposlenik("Benjamin", "Bazdar");
+        System.out.println(u);
+
+    }
+}
