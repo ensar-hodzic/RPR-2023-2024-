e0ddbe0b3a047716851ebf31d1547dbe3d59bfeb
Ensar Hodzic <hodzicensar@gmail.com>
2023-10-31 20:28:22 +0100

    lv1-z1

---
+++ lv1-z1/Z1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Z1{
+ public static void main(String[] args) {
+	int a,b;
+	Scanner input = new Scanner(System.in);
+	System.out.println("Unesite jedan cijeli broj:");
+	a=input.nextInt();
+	System.out.println("Unesite jos jedan:");
+	b=input.nextInt();
+	System.out.println("Prvi broj: "+a+" Drugi broj: "+b);
+ }
+}
