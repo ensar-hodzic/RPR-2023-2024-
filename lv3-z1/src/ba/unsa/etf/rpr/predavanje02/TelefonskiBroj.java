a99c0b31248324456b946c4f131217a4bd3a5963
Ensar Hodzic <hodzicensar@gmail.com>
2023-11-09 16:16:05 +0100

    lv4-z1

--- lv3-z1/src/ba/unsa/etf/rpr/predavanje02/TelefonskiBroj.java
+++
@@ -1,6 +0,0 @@
-package ba.unsa.etf.rpr.predavanje02;
-
-public abstract class TelefonskiBroj {
-    public abstract String ispisi();
-    public abstract int hashCode();
-}
---
+++ lv4-z1/src/main/java/ba/unsa/etf/rpr/Laptop.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr;
+
+public class Laptop {
+    private String brend;
+    private String model;
+    private double cijena;
+    private int ram;
+    private int hdd;
+    private int ssd;
+    private String procesor;
+    private String grafickaKartica;
+    private double velicinaEkrana;
+}
